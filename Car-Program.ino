8f9b851bcc71d7c0b537b8308e83dd17d23f0deb
mabraden <braden.marc@gmail.com>
2024-02-28 09:14:07 -0600

    Update code header/description

--- Car-Program.ino
+++ Car-Program.ino
@@ -1,3 +1,4 @@
+/*This program is used to control the Arduino in a Greenpower electric car, which takes input from sensors and displays (LCD) and records (SD) the information.
 void setup() {
   // put your setup code here, to run once:
 
